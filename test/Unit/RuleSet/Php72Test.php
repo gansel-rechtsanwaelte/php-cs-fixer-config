cc595499ebe76fa72e6e02fa26aa95d0ebf7a8cc
Andreas Möller <am@localheinz.com>
2020-12-11 14:29:12 +0100

    Enhancement: Enable php_unit_namespaced fixer

--- test/Unit/RuleSet/Php72Test.php
+++ test/Unit/RuleSet/Php72Test.php
@@ -264,7 +264,7 @@ final class Php72Test extends AbstractRuleSetTestCase
         'php_unit_method_casing' => true,
         'php_unit_mock' => true,
         'php_unit_mock_short_will_return' => true,
-        'php_unit_namespaced' => false,
+        'php_unit_namespaced' => true,
         'php_unit_no_expectation_annotation' => false,
         'php_unit_set_up_tear_down_visibility' => false,
         'php_unit_size_class' => false,
