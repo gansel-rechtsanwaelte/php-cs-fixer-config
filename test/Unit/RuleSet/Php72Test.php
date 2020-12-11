27af314c6f057e71d54a8f2cc6b26e959764fb66
Andreas Möller <am@localheinz.com>
2020-12-11 12:44:56 +0100

    Enhancement: Enable no_unneeded_final_method fixer

--- test/Unit/RuleSet/Php72Test.php
+++ test/Unit/RuleSet/Php72Test.php
@@ -223,7 +223,7 @@ final class Php72Test extends AbstractRuleSetTestCase
         'no_unneeded_curly_braces' => [
             'namespaces' => true,
         ],
-        'no_unneeded_final_method' => false,
+        'no_unneeded_final_method' => true,
         'no_unreachable_default_argument_value' => false,
         'no_unset_cast' => true,
         'no_unset_on_property' => false,
